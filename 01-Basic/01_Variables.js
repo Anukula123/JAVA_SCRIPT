9d5a3438081adf1a7ce4f19675f1540de34b7201
mr <xyz@gmail.com>
2026-08-02 10:51:24 +0530

    data types in js

---
+++ 01-Basic/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "anukula@123.com";
+var accountPassword = "123";
+accountCity="Paralakhemundi"
+//accountId=1;
+
+//accountId = 12
+accountEmail = "anu@12.com";
+accountPassword = "abc";
+accountCity="Bapujipentho"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity ])
+
---
+++ 01-Basic/02-DataType.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all the js code as new version
+
+let name="Anukula" // string
+let age=12 // int
+let isLoggedIn=false //boolean
+let state=null
+//null ==standalone value or empty
+// undefined means not assign the value
+//Symbol
+// above all are premitive datatypes
+
+//object
+
+console.log(typeof state);
+console.log(typeof age);
+console.log(typeof null); //o/p is object
+console.log(typeof undefined) //o/p undefineds
